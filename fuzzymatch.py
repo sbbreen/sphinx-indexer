0c034560d158c36c2f3e900601caf823ab284be3
Sarah <sburnham5@gmail.com>
2013-12-10 18:28:21 -0500

    Create fuzzymatch.py

---
+++ fuzzymatch.py
@@ -0,0 +1,13 @@
+
+
+
+from nltk import metrics, stem, tokenize
+
+stemmer = stem.PorterStemmer()
+
+def normalize(s):
+    words = tokenize.wordpunct_tokenize(s.lower().strip())
+    return ' '.join([stemmer.stem(w) for w in words])
+
+def fuzzy_match(s1, s2, max_dist=3):
+    return metrics.edit_distance(normalize(s1), normalize(s2)) <= max_dist
